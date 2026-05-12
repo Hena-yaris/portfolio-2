3f2779e386ee268c05dd7715d6834d204f7d4cdc
Hena <meraey21@gmail.com>
2026-05-12 22:12:42 +0300

    Create reusable UI foundation components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+import Button from "../components/ui/Button";
+import Container from "../components/ui/Container";
+import SectionTitle from "../components/ui/SectionTitle";
 
 export default function Home() {
   return (
-     <main className="p-24">
-          <h1 className="text-4xl font-bold">My Portfolio</h1>
-          <p className="mt-4 text-gray-600">Fullstack Developer Intern Candidate</p>
+     <main className="p-32">
+          <Container>
+              <SectionTitle
+               subtitle="Foundation"
+               title= "Building a scalable portfolio"/>
+                    <Button>
+                          Get Started
+                    </Button>
+          </Container>
      </main>
   );
 }
---
+++ src/components/ui/Container.tsx
@@ -0,0 +1,15 @@
+interface ContainerProps {
+  children: React.ReactNode;
+  className?: string;
+
+}
+
+export default function Container({ children, className = "" }: ContainerProps) {
+  return (
+    <div
+      className={`w-full max-w-7xl mx-auto px-6 md:px-10 lg:px-16 ${className}`}
+    >
+      {children}
+    </div>
+  );
+}
